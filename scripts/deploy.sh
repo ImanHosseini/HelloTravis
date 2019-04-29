a18ae99623c4af599ca57ec5eecdcf51d26869b4
Iman <salvationsentinel@yahoo.com>
2019-04-29 18:55:17 +0430

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt-get install sshpass
+ls
 sshpass -p mohr1360@gmail.com scp -o StrictHostKeyChecking=no MVC.jar 93103085@198.144.184.34:/home/93103085/
 sshpass -p mohr1360@gmail.com ssh 93103085@198.144.184.34 nohup java -jar /home/93103085/out/jar/MVC.jar &
