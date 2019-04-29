8e75aa083cbd9eb4a81203fe41564821c4722677
Iman <salvationsentinel@yahoo.com>
2019-04-29 18:07:23 +0430

    a

---
+++ tests/TestSa.java
@@ -0,0 +1,14 @@
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class TestSa {
+
+	@Test
+	public void test_welcome_message() {
+	
+		assertEquals(true, true);
+	}
+ 
+}
