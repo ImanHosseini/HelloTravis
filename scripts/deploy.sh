7af0f25f6d61670942b26e4f6e89b7756347c6ef
Iman <salvationsentinel@yahoo.com>
2019-04-29 19:03:31 +0430

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ cd out/jar
 ls
 pwd
 sshpass -p mohr1360@gmail.com scp -o StrictHostKeyChecking=no MVC.jar 93103085@198.144.184.34:/home/93103085/
-sshpass -p mohr1360@gmail.com ssh 93103085@198.144.184.34 nohup java -jar ./MVC.jar &
+sshpass -p mohr1360@gmail.com ssh 93103085@198.144.184.34 nohup java -jar /home/93103085/MVC.jar &
